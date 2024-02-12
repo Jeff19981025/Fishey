fe662bf00186a06be4dc6b3a927bd9bb0f710d55
Jeff Cai <jeffsonglincai@gmail.com>
2024-02-13 00:44:10 +0800

    Initial Commit

---
+++ Fishey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fishey
+//
+//  Created by 蔡松霖 on 2024/2/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fishey/FisheyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FisheyApp.swift
+//  Fishey
+//
+//  Created by 蔡松霖 on 2024/2/13.
+//
+
+import SwiftUI
+
+@main
+struct FisheyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
